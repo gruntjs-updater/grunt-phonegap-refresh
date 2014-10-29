4cb22f0f907475385ecbf3eff4364ef8adfcbe6a
Chris Thoburn <runspired@gmail.com>
2014-10-29 12:28:23 -0500

    fixes search paths

--- tasks/phonegap_refresh.js
+++ tasks/phonegap_refresh.js
@@ -55,14 +55,14 @@ module.exports = function (grunt) {
 
             //clean the WWW dir but not the config.xml file
             if (options.refreshSource) {
-                cleans.push(PATH_WWW + '**', '!' + PATH_WWW + 'config.xml');
+                cleans.push(PATH_WWW, '!' + PATH_WWW + 'config.xml');
             }
 
             if (options.platforms.indexOf('ios') !== -1) {
                 cleans.push(
-                    PATH_IOS_SPLASH + '*',
-                    PATH_IOS_ICONS + '**',
-                    PATH_IOS_ASSETS + '**',
+                    PATH_IOS_SPLASH,
+                    PATH_IOS_ICONS,
+                    PATH_IOS_ASSETS,
                     '!' + PATH_IOS_ASSETS + 'cordova.js',
                     '!' + PATH_IOS_ASSETS + 'phonegap.js',
                     '!' + PATH_IOS_ASSETS + 'cordova_plugins.js'
@@ -71,9 +71,9 @@ module.exports = function (grunt) {
 
             if (options.platforms.indexOf('android') !== -1) {
                 cleans.push(
-                    PATH_ANDROID_SPLASH + '*',
-                    PATH_ANDROID_ICONS + '**',
-                    PATH_ANDROID_ASSETS + '**',
+                    PATH_ANDROID_SPLASH,
+                    PATH_ANDROID_ICONS,
+                    PATH_ANDROID_ASSETS,
                     '!' + PATH_ANDROID_ASSETS + 'cordova.js',
                     '!' + PATH_ANDROID_ASSETS + 'phonegap.js',
                     '!' + PATH_ANDROID_ASSETS + 'cordova_plugins.js'
